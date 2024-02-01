478cd4066c712829de43ac5a5bc4b1cddbae2295
Alan Lanceloth Rodrigues Silva <alan.lanceloth@gmail.com>
2024-01-31 22:42:02 -0300

    build: transaction contract

---
+++ contract/contract.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class Transaction(BaseModel):
+    transaction_id: str
+    transaction_time: datetime
+    ean: str
+    product_name: str
+    price: float
+    store: int
+    pos_number: int
+    pos_system: str
+    pos_version: float
+    pos_last_maintenance: datetime
+    operator: int
